28a40fd4040081771339a98d8d95fff52da19bae
Ma Chun <chun.ma@qq.com>
2019-12-19 15:38:37 +0800

    fixed: java.lang.IllegalStateException: No instances available for wallet-service

--- src/main/java/net/atos/demo/service/Constant.java
+++ src/main/java/net/atos/demo/service/Constant.java
@@ -2,7 +2,7 @@ package net.atos.demo.service;
 
 public class Constant {
 	
-    public static final String APPID = "order-service";
+    public static final String APPID = "order";
     
 	public static final String BUSINESS_CODE = "buySth";
 }
